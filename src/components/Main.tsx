a7bea5b0ad95c7ba85ffa48ecc034fc4e14106ca
abby.low <abby.low@bybit.com>
2022-05-24 00:20:20 +0800

    feat: get and show ray balance

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -33,14 +33,16 @@ import {
   SOL_IMG,
   RAY_IMG,
   RAY_SOL_LP_V4_POOL_KEY,
-  RAYDIUM_LIQUIDITY_JSON
+  RAYDIUM_LIQUIDITY_JSON,
+  RAY_TOKEN_MINT
 } from '../constant';
 
 const Main: FC = () => {
-  const { publicKey, sendTransaction, signTransaction } = useWallet();
+  const { publicKey, sendTransaction } = useWallet();
   const { connection } = useConnection();
 
   const [solBalance, setSolBalance] = useState<number>(0);
+  const [rayBalance, setRayBalance] = useState<number>(0);
   const [exchangeRate, setExchangeRate] = useState<string>('');
 
   const [raySolPoolKey, setRaySolPoolKey] = useState<LiquidityPoolKeys>();
@@ -62,6 +64,15 @@ const Main: FC = () => {
 
         const tokenAccs = await getTokenAccountsByOwner(connection, publicKey as PublicKey); // get all token accounts
         setTokenAccounts(tokenAccs);
+
+        let rayTokenAddress: PublicKey;
+        tokenAccs.filter(acc => acc.accountInfo.mint.toBase58() === RAY_TOKEN_MINT).map(async(acc) => {
+          rayTokenAddress = acc.pubkey;
+          const accBalance = await connection.getTokenAccountBalance(rayTokenAddress);
+          const rayBal = accBalance.value.uiAmount || 0;
+          setRayBalance(rayBal);
+        });
+        
       }
     };
     const getPoolInfo = async () => {
@@ -151,7 +162,7 @@ const Main: FC = () => {
                     <b>Input</b>
                   </label>
                   <span className="float-end text-muted">
-                    {`Balance: ${solBalance.toFixed(5)} SOL`}
+                    {`Balance: ${solBalance.toFixed(9)} SOL`}
                   </span>
                 </div>
                 <div className="input-group mb-4">
@@ -170,9 +181,9 @@ const Main: FC = () => {
                 </div>
                 <div>
                   <label className="float-start"><b>Estimated Output</b></label>
-                  {/* <span className="float-end text-muted"> */}
-                  {/* {`Balance: ${rayBalance.toFixed(5)}`} RAY*/}
-                  {/* </span> */}
+                  <span className="float-end text-muted">
+                    {`Balance: ${rayBalance.toFixed(6)}`} RAY
+                  </span>
                 </div>
                 <div className="input-group mb-2">
                   <input
